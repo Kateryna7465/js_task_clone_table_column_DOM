ac8d7ef5fcf3be20c256014dc137fea184f6a1af
kateryna Habchak <kvizovapidtrimka@gmail.com>
2026-07-15 14:06:40 +0200

    make build

---
+++ index.70e9ef84.js
@@ -0,0 +1,2 @@
+document.querySelectorAll("table tr").forEach(function(e){var t=e.querySelectorAll("th, td");if(t.length>=2){var l=t[1].cloneNode(!0),r=t[t.length-1];e.insertBefore(l,r)}});
+//# sourceMappingURL=index.70e9ef84.js.map
